959d62de1520e245d2a1da925953efd5c7424b9e
Matt Piatkus <mattpiatkus@gmail.com>
2023-11-26 16:41:04 +0000

    Refactor from procedural to word2vec processor class

--- process_phrases.py
+++ process_phrases.py
@@ -1,53 +1,10 @@
-from itertools import combinations
 import json
 
-import numpy as np
 from gensim.models import KeyedVectors
-from gensim.utils import tokenize
 import polars as pl
 
-# Version 1 of this needs multiple upgrads:
-# 1. Assign ids to each of the phrases
-# 2. Use Gensim to get vectorised all phrases against all instead of python combinations
+from processors.word2vec_processor import Word2VecProcessor
 
-
-def phrase_to_vector(keyed_vectors: KeyedVectors, phrase: str) -> np.ndarray:
-    vector = keyed_vectors.get_mean_vector(keys=list(phrase))
-
-    return vector
-
-
-def phrase_pair_to_distance(keyed_vectors: KeyedVectors, phrase_pair: (str, str)) -> float:
-    # This function can have its efficiency improved by using Gensim to compare each to each other.
-    # Open a story for this optimisation
-
-    left_vector = phrase_to_vector(keyed_vectors, phrase_pair[0])
-
-    right_vector = phrase_to_vector(keyed_vectors, phrase_pair[1])
-
-    # Gensim cosine similarities compares 1 vector to many, so right side must be a matrix
-    right_matrix = right_vector.reshape(1, len(right_vector))
-
-    cosine_similarities = keyed_vectors.cosine_similarities(left_vector, right_matrix)
-
-    return cosine_similarities[0].item()
-
-
-def phrase_list_to_all_distances(keyed_vectors: KeyedVectors, phrase_list: list[str])\
-        -> dict[tuple[str, str], np.float32]:
-
-    phrase_vectors = {}
-
-    for index, pair in enumerate(combinations(phrase_list, 2)):
-        distance = phrase_pair_to_distance(keyed_vectors, pair)
-
-        phrase_details = {}
-        phrase_details['pair'] = pair
-        phrase_details['distance'] = distance
-
-        phrase_vectors[index] = phrase_details
-
-    return phrase_vectors
 
 
 if __name__ == "__main__":
@@ -55,10 +12,15 @@ if __name__ == "__main__":
 
     phrases = pl.read_csv('data/phrases.csv', encoding='windows-1252')
 
-    phrase_vectors = phrase_list_to_all_distances(keyed_vectors=kv,
-                                                  phrase_list=list(phrases['Phrases']))
+    phrase_list = list(phrases['Phrases'])
+
+    processor = Word2VecProcessor(keyed_vectors=kv, phrase_list=phrase_list)
+
+    processor.calculate_all_phrase_distances()
+
+    all_phrase_distances = processor.all_phrase_distances
 
     # The output format was not specified in requirements - output a json for now
-    with open('data/phrase_vectors.json', 'w') as f:
-        json.dump(phrase_vectors, f)
+    with open('data/phrase_pair_vectors.json', 'w') as f:
+        json.dump(all_phrase_distances, f)
 
---
+++ processors/word2vec_processor.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass, field
+from itertools import combinations
+from typing import Optional
+
+import numpy as np
+from gensim.models import KeyedVectors
+
+from process_phrases import phrase_list
+
+
+@dataclass
+class Word2VecProcessor:
+    """
+    Holds word-to-vec keyed vectors and a phrase list and allows processing of phrases
+    and comparison of new phrases against the list
+    """
+    keyed_vectors: KeyedVectors
+    phrase_list: list
+
+    indexed_phrases: Optional[dict[int, str]]  = field(default_factory=dict)
+    phrase_vectors: Optional[np.ndarray] = np.ndarray
+    all_phrase_distances: Optional[dict[int, dict[tuple[str, str], float]]] = field(default_factory=dict)
+
+    def __post_init__(self):
+        # Create a lookup of the phrases
+        self.indexed_phrases = {index: phrase for index, phrase in enumerate(self.phrase_list)}
+
+        # Vectorise all the phrases on class initiation.
+        # TODO is this correct?  Might want to leave this till initiated by the user
+        vectors = []
+        for phrase in self.phrase_list:
+            vectors.append(self.phrase_to_vector(phrase))
+
+        self.phrase_vectors = np.vstack(vectors)
+
+    def phrase_to_vector(self, phrase: str) -> np.ndarray:
+        vector = self.keyed_vectors.get_mean_vector(keys=list(phrase))
+
+        return vector
+
+    def phrase_pair_to_distance(self, phrase_pair: (str, str)) -> float:
+        # Instead of comparing phrases in pairs, efficiency could be increased by
+        # using gensim and numpy to compare each phrase to each other (or each previous)
+
+        left_vector = self.phrase_to_vector(phrase_pair[0])
+
+        right_vector = self.phrase_to_vector(phrase_pair[1])
+
+        # Gensim cosine similarities compares 1 vector to many, so right side must be a matrix
+        right_matrix = right_vector.reshape(1, len(right_vector))
+
+        cosine_similarities = self.keyed_vectors.cosine_similarities(left_vector, right_matrix)
+
+        return cosine_similarities[0].item()
+
+    def calculate_all_phrase_distances(self) -> None:
+        """
+        Calculates the distances between all phrases on the object
+        Order matters - the first phrase appearing in the original data source will come first in the pair
+        Therefore a lookup might need to reverse the pairs to find the distance
+        Creates a new integer index for each pair
+        TODO is this really the best data structure?
+        :return: dict like {0, {'pair': ('I like cake',"cake is nice"), "distance": 0.99}}
+        """
+        phrase_distances = {}
+
+        for index, pair in enumerate(combinations(phrase_list, 2)):
+            distance = self.phrase_pair_to_distance(pair)
+
+            phrase_details = {'pair': pair, 'distance': distance}
+
+            phrase_distances[index] = phrase_details
+
+        self.all_phrase_distances = phrase_distances
+
+    def closest_phrase(self,
+                       input_phrase: str) -> tuple[int, str]:
+        """
+        Computes the closest phrase in phrase_vectors to the input phrase
+        :param phrase_vectors: dict where keys are phrases and values their vectors
+        :param input_phrase: str
+        :return: The closest phrase to the input phrase
+        """
+        input_vector = self.phrase_to_vector(self.keyed_vectors, input_phrase)
+        similarities = self.keyed_vectors.cosine_similarities(input_vector, self.phrase_vectors)
+        top_similarity_index = np.argmax(similarities)
+
+        return top_similarity_index, self.indexed_phrases[top_similarity_index]
